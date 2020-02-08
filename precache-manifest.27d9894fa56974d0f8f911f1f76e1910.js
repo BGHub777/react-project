9c5861a9543fdee782d1b29a954fe001b08327e7
Nobro777 <52586100+Nobro777@users.noreply.github.com>
2020-02-07 22:49:37 -0800

    Updates

---
+++ precache-manifest.27d9894fa56974d0f8f911f1f76e1910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfee619a805010f2a02bfe4343ae31c8",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "45a02b8560623749916c",
+    "url": "/React-Project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dc4e46b9b87e34aaeb65",
+    "url": "/React-Project/static/js/2.41ac8598.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Project/static/js/2.41ac8598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a02b8560623749916c",
+    "url": "/React-Project/static/js/main.95726897.chunk.js"
+  },
+  {
+    "revision": "dab84bad56c716359ef8",
+    "url": "/React-Project/static/js/runtime-main.37a84aa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
